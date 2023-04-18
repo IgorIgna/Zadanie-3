60bf4baf6f7716ae15c183451c896e342ad40378
IgorIgna <130740014+IgorIgna@users.noreply.github.com>
2023-04-18 14:46:33 +0200

    Update admin.sql

--- admin.sql
+++ admin.sql
@@ -1,3 +1,4 @@
 CREATE TABLE `users` (`id` INT(11) NOT NULL AUTO_INCREMENT , `username` VARCHAR(30) NOT NULL , `password` VARCHAR(30) NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
+ALTER TABLE `users` ADD UNIQUE(`username`);
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES (NULL, 'amdmin', 'test');
